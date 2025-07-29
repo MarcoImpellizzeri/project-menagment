fcb1284c81003c9dafc0f099876bb4d8f2485ff2
Marco Impellizzeri <impellizzeri.marco18@gmail.com>
2025-07-29 14:53:48 +0200

    aggiunto componente NoProjectSelected e Button, aggiornato App per gestire lo stato del progetto

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,38 @@
 import ProjectSidebar from './Components/ProjectSidebar.jsx'
 import NewProject from './Components/NewProject.jsx'
+import NoProjectSelected from './Components/NoProjectSelected.jsx'
+import { useState } from 'react'
 
 function App() {
+  const [projectState, setProjectState] = useState({
+    selectedProjectId: undefined,
+    projects: []
+  })
+
+  function handleStartAddProject(){
+    console.log('start add project')
+    setProjectState( prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: null
+      }
+    })
+  }
+
+  let content
+
+  if (projectState.selectedProjectId === null) {
+    console.log('primo if')
+    content = <NewProject />
+  } else if (projectState.selectedProjectId === undefined) {
+    console.log('secondo if')
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject}/>
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectSidebar />
-      <NewProject />
+      <ProjectSidebar onStartAddProject={handleStartAddProject}/>
+      {content}
     </main>
   );
 }
---
+++ src/Components/NoProjectSelected.jsx
@@ -0,0 +1,19 @@
+import noProjectImage from '/src/assets/no-projects.png'
+import Button from './Button.jsx'
+
+export default function NoProjectSelected({ onStartAddProject }) {
+  return (
+    <div className="mt-24 text-center w-2/3">
+      <img
+        src={noProjectImage}
+        alt="no project"
+        className="mx-auto w-16 h-16 object-contain"
+      />
+      <h2 className="text-xl font-bold text-stone-500 my-4">No Project Selected</h2>
+      <p>Select a project or get started with a new one</p>
+      <p className="mt-8">
+        <Button children="Create new project" onClick={onStartAddProject}/>
+      </p>
+    </div>
+  )
+}
